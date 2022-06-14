9fe5f3f9342d6e3c3b847e3274297075b3f701bf
Sarah Touzani <touzani.sarah@hotmail.com>
2022-06-14 13:51:54 +0200

    feat(back-database): Ajoute la base Entreprises
    what:
    Ajout d'une base permettant d'avoir une liste d'entreprises par départements avec leur type de profession.

---
+++ back-spring-boot/src/main/java/com/groupe2/backspringboot/repository/EntreprisesRepository.java
@@ -0,0 +1,9 @@
+package com.groupe2.backspringboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.groupe2.backspringboot.model.Entreprises;
+
+public interface EntreprisesRepository extends JpaRepository<Entreprises, Integer>{
+
+}
